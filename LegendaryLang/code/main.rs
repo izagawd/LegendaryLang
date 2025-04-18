e3587e8b2eb8e90ad0d4e8e06540dca14cfc4985
Emmanuel Onyekwelu <30781523+izagawd@users.noreply.github.com>
2025-04-18 17:34:11 -0300

    aded math

--- LegendaryLang/code/main.rs
+++ LegendaryLang/code/main.rs
@@ -3,17 +3,22 @@ use code::Human;
 use code::dark::Nester;
  use std::primitive::i32;
 
-
+struct Many{
+    a : i32,
+    b : i32,
+    c: i32,
+    d: i32,
+    e: i32,
+    f: i32,
+    g: i32,    
+}
 fn main() -> i32{
     code::main2()
 
 }
 
 fn main2() -> i32{
-    let a = code::Human{
-            age = 1 + 1
-        };
-    a.age = a.age * 2;
-    a.age
+    let a = true;
+    5
 }
 
