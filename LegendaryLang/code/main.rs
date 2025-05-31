3049466fcdfcfb691d9f6f41939484b9b29b09d7
Emmanuel Onyekwelu <30781523+izagawd@users.noreply.github.com>
2025-05-31 04:41:11 -0300

    changed some naming

--- LegendaryLang/code/main.rs
+++ LegendaryLang/code/main.rs
@@ -1,25 +1,14 @@
-﻿
-use code::Human;
-use code::hello;
+﻿use std::primitive::i32;
 
-use std::primitive::i32;
-use code::make_human_with_age;
-use code::bruh;
-use code::other;
-use code::Dick;
-use code::Hey;
-use code::Ola;
 
 fn main() -> i32{
     {
-             let bruh = if true{
-                 5
-                 } else{
-                     10
-             };
-             return bruh;
-                bruh
+        
+        if true{
+            return 5;
+            } else{
+                return 5;
+                }
         }
-
 }
 
