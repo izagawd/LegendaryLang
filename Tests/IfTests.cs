6c5d87e36a63adc81efc2f723296f2d681d8ec33
Emmanuel Onyekwelu <30781523+izagawd@users.noreply.github.com>
2025-06-03 21:48:37 -0300

    added Tests

---
+++ Tests/IfTests.cs
@@ -0,0 +1,18 @@
+﻿using LegendaryLang;
+using LegendaryLang.Parse;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class IfTests
+{
+    [Test]
+    public void IfImplicitReturnTest()
+    {
+        var compiled =new Compiler().Compile("compiler_tests/if_tests/if_implicit_return",false,false);
+        if (compiled() != 9)
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ Tests/compiler_tests/if_tests/if_implicit_return/main.rs
@@ -0,0 +1,18 @@
+﻿
+use std::primitive::i32;
+fn main() -> i32{
+    let gotten = if true{
+        5
+        }
+    else{
+        10
+        };
+        
+    let gotten2 = if false{
+            7
+        }   else{
+            4
+            };
+            
+    gotten + gotten2  
+}
