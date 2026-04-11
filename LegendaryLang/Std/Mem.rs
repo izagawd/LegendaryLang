9aa05eccedff65d3eb989a9b3dac4a898549ce6e
theiz <theizaboss@gmail.com>
2026-04-11 18:54:10 -0300

    Update manuallydrop things

--- LegendaryLang/Std/Mem.rs
+++ LegendaryLang/Std/Mem.rs
@@ -9,4 +9,16 @@ impl[T:! Sized] ManuallyDrop(T) {
     fn New(val: T) -> ManuallyDrop(T) {
         make ManuallyDrop { val: val }
     }
+
+    fn inner_ref(self: &Self) -> &T {
+        &self.val
+    }
+
+    fn inner_mut(self: &mut Self) -> &mut T {
+        &mut self.val
+    }
+
+    fn inner(self: Self) -> T {
+        self.val
+    }
 }
